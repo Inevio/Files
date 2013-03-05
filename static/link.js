e4387e39ef681e821192d4c60aecbbd668a1dc28
javiergarmon <jgarmon@weezeel.com>
2013-03-05 10:35:54 +0100

    [Implementation] Support to boolean preview in createLink()

--- static/link.js
+++ static/link.js
@@ -1,56 +1,32 @@
 
 wz.app.addScript( 1, 'link', function( win ){
-	
-	var file = {};
-	var linkSpan = $('.link-url .wz-selectable', win);
 
-	win
-	
-		.on( 'app-param', function( e, params ){
-			
-			wz.structure( params, function( error, structure ){
-				
-				file = structure;
-				
-			});
-			
-		})
-	
-		.on( 'click', '.link-password button', function(){
-				
-			var url 		= '';
-			var password 	= $('.link-password input', win).val();
-			
-			file.getLinks( function( error, list ){
-				
-				if( password.length || !list.length ){
-										
-					file.createLink( password, true, function( error, url ){
-						
-						linkSpan.text( url.url );
-						
-					})
-					
-				}else{
-					
-					if( list[0].password ){
-												
-						file.createLink( null, true, function( error, url ){
-						
-							linkSpan.text( url.url );
-						
-						})
-						
-					}else{
-												
-						linkSpan.text( list[0].url );
-						
-					}
-					
-				}
-				
-			})
-			
-		});
+    var file = {};
+    var linkSpan = $('.link-url .wz-selectable', win);
+
+    win
+    .on( 'app-param', function( e, params ){
+
+        wz.structure( params, function( error, structure ){
+            file = structure;
+        });
+
+    })
+
+    .on( 'click', '.link-password button', function(){
+
+        var url      = '';
+        var password = $('.link-password input', win ).val();
+        var preview  = !$('.link-preview input', win ).prop('checked');
+        
+        if( !password.length ){
+            password = null;
+        }
+
+        file.createLink( password, preview, function( error, url ){
+            linkSpan.text( url.url );
+        });
+
+    });
 
 });
