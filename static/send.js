18ab754fdf75f1549f946bdcf91ad910b344b7be
Robby2012 <robby.upm@gmail.com>
2013-04-24 18:58:32 +0200

    weeXplorer - sendFile promises

--- static/send.js
+++ static/send.js
@@ -22,19 +22,32 @@ wz.app.addScript( 1, 'send', function( win, app, lang, params ){
             if( sendChosenUsers.children().size() ){
 
                 wz.structure( params, function( error, structure ){
+
+                    var promises = [];
                 
                     sendChosenUsers.children().each( function(){
-                        structure.sendTo( $(this).data( 'user-id' ), $( '.send-message', win).val() );
+
+                        var deferred  = $.Deferred();
+                    
+                        promises.push( deferred.promise() );
+
+                        structure.sendTo( $(this).data( 'user-id' ), $( '.send-message', win).val(), function( error ){
+                            deferred.resolve( error );
+                        });
+
                     });
 
-                    wz.banner()
-                    .title( lang.fileSent )
-                    .text( structure.name + ' ' + lang.fileSentCorrectly )
-                    .image( structure.icons.tiny )
-                    .render();
-                        
-                    wz.app.closeWindow( win.data( 'win' ) );
+                    $.when.apply( null, promises ).then( function(){
 
+                        wz.banner()
+                            .title( lang.fileSent )
+                            .text( structure.name + ' ' + lang.fileSentCorrectly )
+                            .image( structure.icons.tiny )
+                            .render();
+                                
+                        wz.app.closeWindow( win.data( 'win' ) );
+
+                    });
                 
                 });
 
