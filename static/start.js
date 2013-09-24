de42d78d1c137316190cce1d215ca30202fedfd4
javiergarmon <jgarmon@weezeel.com>
2013-09-24 17:59:29 +0200

    weeXplorer - Initial display was misplaced

--- static/start.js
+++ static/start.js
@@ -7,32 +7,42 @@ wql.getConfig( function( error, result ){
 
         result = result[ 0 ];
 
-        // Redimensionamos la ventana
-        wz.fit( win, result.width - win.width(), result.height - win.height() );
-
         // Guardamos la configuración
         app.sortType = result.sort;
         app.sidebar  = Boolean( result.sidebar );
-
-        // Añadimos el sidebar si está activado
-        if( result.sidebar ){
-
-            win.addClass('sidebar');
-
-            var sidebarWidth = $( '.weexplorer-sidebar', win ).outerWidth( true );
-
-            $( '.weexplorer-main', win ).width( '-=' + sidebarWidth + 'px' );
-            $( '.weexplorer-folder', win ).width( '-=' + sidebarWidth + 'px' );
-            $( '.weexplorer-file-zone', win ).width( '-=' + sidebarWidth + 'px' );
-
-            app.sortType = result.sort;
-            app.viewType = result.view;
-        
-        }
+        app.viewType = result.view;
 
     }else{
 
         wql.insertConfig();
+
+        // Guardamos la configuración
+        app.sortType = 0;
+        app.sidebar  = 1;
+        app.viewType = 0;
+
+        result = {
+
+            height : win.height(),
+            width  : win.width()
+
+        };
+
+    }
+
+    // Redimensionamos la ventana
+    wz.fit( win, result.width - win.width(), result.height - win.height() );
+
+    // Añadimos el sidebar si está activado
+    if( app.sidebar ){
+
+        win.addClass('sidebar');
+
+        var sidebarWidth = $( '.weexplorer-sidebar', win ).outerWidth( true );
+
+        $( '.weexplorer-main', win ).width( '-=' + sidebarWidth + 'px' );
+        $( '.weexplorer-folder', win ).width( '-=' + sidebarWidth + 'px' );
+        $( '.weexplorer-file-zone', win ).width( '-=' + sidebarWidth + 'px' );
 
     }
 
