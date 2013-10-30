4c40924f51df7354b48f4f420eecb1a638a62372
Robby2012 <robby.upm@gmail.com>
2013-10-30 21:06:27 +0100

    weeXplorer - Started Mobile

---
+++ static/mobile/script.js
@@ -0,0 +1,30 @@
+
+/*var body = $( document.body );
+
+$( function(){
+
+    $( '#weexplorer-menu-sidebar' ).on( 'tap', function(){
+
+        $( '#weexplorer-sidebar' ).transition({ left : 0 }, 200, function(){
+            body.addClass( 'sidebar' );
+        });
+
+    });
+
+    $( '#weexplorer-sidebar' ).on( 'tap', function( e ){
+        e.stopPropagation();
+    });
+
+    body.on( 'tap', function(){
+
+        if( body.hasClass( 'sidebar' ) ){
+
+            $( '#weexplorer-sidebar' ).transition({ left : '-80%' }, 200, function(){
+                body.removeClass( 'sidebar' );
+            });
+
+        }
+
+    });
+
+});*/
