e5c82f4fbfa0fd74f5a115473333b5991463f710
Robby2012 <robby.upm@gmail.com>
2013-10-31 12:04:49 +0100

    weeXplorer - First mobile version

--- static/mobile/script.js
+++ static/mobile/script.js
@@ -1,30 +1,27 @@
 
-/*var body = $( document.body );
+var win = $( this );
 
-$( function(){
+$( '#weexplorer-menu-sidebar' ).on( 'tap', function(){
 
-    $( '#weexplorer-menu-sidebar' ).on( 'tap', function(){
+    $( '#weexplorer-sidebar' ).transition({ left : 0 }, 200, function(){
+        win.addClass( 'sidebar' );
+    });
 
-        $( '#weexplorer-sidebar' ).transition({ left : 0 }, 200, function(){
-            body.addClass( 'sidebar' );
+});
+
+$( '#weexplorer-sidebar' ).on( 'tap', function( e ){
+    e.stopPropagation();
+});
+
+win.on( 'tap', function(){
+
+    if( win.hasClass( 'sidebar' ) ){
+
+        $( '#weexplorer-sidebar' ).transition({ left : '-80%' }, 200, function(){
+            win.removeClass( 'sidebar' );
         });
 
-    });
+    }
 
-    $( '#weexplorer-sidebar' ).on( 'tap', function( e ){
-        e.stopPropagation();
-    });
+});
 
-    body.on( 'tap', function(){
-
-        if( body.hasClass( 'sidebar' ) ){
-
-            $( '#weexplorer-sidebar' ).transition({ left : '-80%' }, 200, function(){
-                body.removeClass( 'sidebar' );
-            });
-
-        }
-
-    });
-
-});*/
