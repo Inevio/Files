b149b54bcc20ac9b29adbc44a41e3556b9117aa9
Javier Garmon <jgarmon@inevio.com>
2016-06-11 14:38:41 +0200

    Implementing Share view

--- static/share.js
+++ static/share.js
@@ -1,4 +1,33 @@
 
+// Variables
+var visualUsersList              = $('.users-list');
+var visualUsersListUserPrototype = $('.users-list .user.wz-prototype');
+
+// Functions
+var appendUserToUsersList = function( user ){
+
+  var newUser = visualUsersListUserPrototype.clone().removeClass('wz-prototype');
+
+  newUser.find('.avatar').attr( 'src', user.avatar.small );
+  newUser.find('.name').text( user.fullName );
+  visualUsersList.append( newUser );
+
+};
+
+// Events
 $('.permission .icon').on( 'click', function(){
   $(this).parent('.permission').toggleClass('active');
+});
+
+// Start the app
+api.user.friendList( false, function( error, list ){
+  list.forEach( appendUserToUsersList );
+});
+
+wz.fs( params, function( error, fsnode ){
+
+  fsnode.sharedWith( function(){
+    console.log( arguments );
+  });
+
 });
