0be382dbe7a88b26fa68da5e60727f06ee482e43
Javier Garmón <jgarmon@inevio.com>
2016-08-29 15:42:39 +0200

    Improvements in selectSource and selectDestiny

--- static/start.js
+++ static/start.js
@@ -1,10 +1,10 @@
 
 if( params ){
 
-  if( params.command === 'selectPath' ||  params.command === 'selectFile' ){
-    $(this).addClass( 'select ' + params.command );
-  }else if( params.command === 'saveFile' ){
-    $(this).addClass('save');
+  if( params.command === 'selectSource' ){
+    $(this).addClass( 'selectSource ' + params.mode );
+  }else if( params.command === 'selectDestiny' ){
+    $(this).addClass('selectDestiny');
   }
 
 }
