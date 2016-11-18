bacccabbc2ee1fd807361949932bf5727699b4e4
Jorge Fernandez <jorge92f@gmail.com>
2016-11-18 17:09:52 +0100

    Badge error fix

--- static/service.js
+++ static/service.js
@@ -1,83 +1,52 @@
-var SHARED_PATH = 0;
+var SHARED_FOLDER = 0;
 
-var updateBadge = function( num , add ){
+var setBadge = function(){
 
-  var actualBadge = wz.app.getBadge();
+  SHARED_FOLDER.list( function( e , files ){
 
-  if ( add ) {
-    wz.app.setBadge( parseInt(actualBadge) + num  );
-  }else{
-    wz.app.setBadge( parseInt(actualBadge) - num  );
-  }
+    wz.app.setBadge( files.length );
 
-
-};
-
-var updateNotificationBadge = function( fsnode , options ){
-
-  var fsnodeId = options.onlyId ? fsnode : fsnode.id;
-
-  if ( options.isNew && SHARED_PATH === fsnode.parent ) {
-
-    updateBadge( 1 , true );
-
-  }else{
-
-    if ( options.parent && options.parent === SHARED_PATH ) {
-
-      updateBadge( 1 , false );
-
-    }else if( options.origin && options.origin === SHARED_PATH ){
-
-      updateBadge( 1 , false );
-
-    }
-
-  }
+  });
 
 }
 
 var checkRecieved = function(){
 
-  api.fs( 'root', function( error, fsnode ){
+  if ( SHARED_FOLDER === 0 ) {
+    api.fs( 'root', function( error, fsnode ){
 
-    fsnode.list( function( error, folders ){
+      fsnode.list( function( error, folders ){
 
-      folders = folders.filter( function( folder ){
-        return folder.type === 1 && folder.name === 'Received';
-      })
+        folders = folders.filter( function( folder ){
+          return folder.type === 1 && folder.name === 'Received';
+        })
 
-      SHARED_PATH  = folders[0].id;
-      folders[0].list( function( e , files ){
-
-        files.forEach( function(){
-
-          updateBadge( 1 , true );
-
-        });
+        SHARED_FOLDER  = folders[0];
+        setBadge();
 
       });
 
     });
-
-  });
+  }else{
+    setBadge();
+  }
 
 }
 
 api.fs
 .on( 'new', function( fsnode ){
 
-  updateNotificationBadge( fsnode , { isNew: true , onlyId: false } );
+  checkRecieved();
 
 })
 .on( 'move', function( fsnode, finalDestiny, originalSource ){
 
-  updateNotificationBadge( fsnode , { isNew: false , onlyId: false , origin: originalSource } );
+  checkRecieved();
 
 })
 .on( 'remove', function( fsnodeId, quota, parent ){
 
-  updateNotificationBadge( fsnodeId , { isNew: false , onlyId: true , parent: parent } );
+  checkRecieved();
 
 })
 
