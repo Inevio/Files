a620ba41136c2bfbbbcdee93010507e204ef44cf
Carlos Navas <carlosnavas1991@gmail.com>
2017-06-26 11:45:57 +0200

    Removing arrow

--- static/script.js
+++ static/script.js
@@ -2521,6 +2521,7 @@ var startOnboarding = function(){
 
   $('.ui-content .welcome-tip').show();
   var contextTimeout = setTimeout( function(){ $('.context-menu-reminder').show(); }, 300000 );//5 min
+  $( '.onboarding-arrow.files' , window.document ).remove();
 
 }
 
