64d83bd378a88d6cd71704f60f612c9f4cf7ed66
Carlos Navas <carlosnavas1991@gmail.com>
2017-09-14 15:31:47 +0200

    Cleaning code

--- static/inviteByMail.js
+++ static/inviteByMail.js
@@ -134,14 +134,18 @@ window.fbAsyncInit = function() {
 
 };
 
-// Load the SDK asynchronously 
-(function(d, s, id) { 
-  var js, fjs = d.getElementsByTagName(s)[0]; 
-  if (d.getElementById(id)) return; 
-  js = d.createElement(s); js.id = id; 
-  js.src = "//connect.facebook.net/en_US/sdk.js"; 
-  fjs.parentNode.insertBefore(js, fjs); 
-}(window.document, 'script', 'facebook-jssdk')); 
+if( typeof FB == 'undefined' ){
+
+  // Load the SDK asynchronously 
+  (function(d, s, id) { 
+    var js, fjs = d.getElementsByTagName(s)[0]; 
+    if (d.getElementById(id)) return; 
+    js = d.createElement(s); js.id = id; 
+    js.src = "//connect.facebook.net/en_US/sdk.js"; 
+    fjs.parentNode.insertBefore(js, fjs); 
+  }(window.document, 'script', 'facebook-jssdk')); 
+
+}
 
 generateGmailUrl();
 initText();
