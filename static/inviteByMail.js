32868644ab5ed56f9679dd55fad7ba869bbc4d8b
Carlos Navas <carlosnavas1991@gmail.com>
2017-09-14 15:29:59 +0200

    Fix

--- static/inviteByMail.js
+++ static/inviteByMail.js
@@ -134,5 +134,14 @@ window.fbAsyncInit = function() {
 
 };
 
+// Load the SDK asynchronously 
+(function(d, s, id) { 
+  var js, fjs = d.getElementsByTagName(s)[0]; 
+  if (d.getElementById(id)) return; 
+  js = d.createElement(s); js.id = id; 
+  js.src = "//connect.facebook.net/en_US/sdk.js"; 
+  fjs.parentNode.insertBefore(js, fjs); 
+}(window.document, 'script', 'facebook-jssdk')); 
+
 generateGmailUrl();
 initText();
