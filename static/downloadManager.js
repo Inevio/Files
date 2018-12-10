a8b5ebcfd261a836f96bf36abc680a8f68d91af5
Carlos Navas <carlosnavas1991@gmail.com>
2018-12-10 17:49:14 +0100

    WiP

--- static/downloadManager.js
+++ static/downloadManager.js
@@ -1,3 +1,36 @@
-/*api.fs.on('upload', function(){
+var uploadPrototype = $('.file-info.wz-prototype')
+var win = $(this)
+
+var uploadProgress = function(fsnodeID, progress){
+  $('.file-info.upload-' + fsnodeID).find('file-progress').text(progress*100 + '%')
+}
+
+api.upload
+  .on('conflict', function(data){
+    alert(data.origin + ' ' + lang.alreadyExists + ' ' + lang.destinyFolder)
+  })
+  .on('fileEnqueued', function (file, queue) {
   
-})*/
+  })
+  .on('fsnodeStart', function (fsnode, queue) {
+    let queueSize = queue.length()
+    let uploadDom = uploadPrototype.clone().removeClass('.wz-prototype').addClass('uploadDom')
+    uploadDom.addClass('upload-' + fsnode.id)
+    uploadDom.find('.name').text(fsnode.name)
+    uploadDom.find('.file-size').text(fsnode.size)
+    uploadDom.find('file-progress').text(lang.pending)
+    $('.content').append(uploadDom)
+  })
+
+  .on('fsnodeProgress', function (fsnodeID, progress, queue) {
+    uploadProgress(fsnodeID, progress)
+  })
+
+  .on('fsnodeQueueEnd', function () {
+    //TODO closeApp
+    api.app.closeApp()
+  })
+
+win.on('click', '.see-more', function(){
+  win.toggleClass('contracted')
+})
